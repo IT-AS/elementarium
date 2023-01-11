4e0c37819484a241434f5c9458eb9dcae8a6919b
Artur Savitskiy <info@it-as.eu>
2023-01-11 14:26:30 +0100

    WIP

--- client/src/app/shared/components/footer/footer.component.ts
+++ client/src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'elementarium-footer',
@@ -7,10 +7,25 @@ import {Component, OnInit} from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  private baseSize: number;
+
+  public get footerWidth(): number {
+    return this.baseSize * 0.9;
+  }
+
+  public get footerHeight(): number {
+    return this.baseSize * 0.05;
+  }
+
   constructor() {
+    this.getScreenSize();
   }
 
   ngOnInit(): void {
   }
 
+  @HostListener('window:resize', ['$event'])
+  private getScreenSize(event?) {
+    this.baseSize = Math.min(window.innerHeight, window.innerWidth);
+  }
 }
--- client/src/app/shared/components/header/header.component.ts
+++ client/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'elementarium-header',
@@ -7,10 +7,25 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  private baseSize: number;
+
+  public get headerWidth(): number {
+    return this.baseSize * 0.9;
+  }
+
+  public get headerHeight(): number {
+    return this.baseSize * 0.05;
+  }
+
   constructor() {
+    this.getScreenSize();
   }
 
   ngOnInit(): void {
   }
 
+  @HostListener('window:resize', ['$event'])
+  private getScreenSize(event?) {
+    this.baseSize = Math.min(window.innerHeight, window.innerWidth);
+  }
 }
